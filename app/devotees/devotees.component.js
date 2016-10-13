a3f6f4668946eb1086c2a2e7b342c441ee58c71d
nirdosh <parmar.nikunjkumar@gmail.com>
2016-10-13 22:30:30 +0200

    component updated with http json

--- app/devotees/devotees.component.js
+++ app/devotees/devotees.component.js
@@ -2,27 +2,14 @@ angular.module('devotees')
 	.component('devotees',{
 		templateUrl: 'devotees/devotees.template.html',
         controller: function PhoneListController($scope, $http){
-			this.devotees = {};
-			$http.get(
-					  "http://localhost:8080/devotee"
-
-
-			).then(
-				function (success) {
-					console.log(success);
-					$scope.devotess = success.data;},
-				function (error) {
-				devotess = [
-					{
-						"name":"manibhai"
+			var self = this;
+			$http.get("http://localhost:8080/devotee")
+				.then(
+					function (success) {
+						self.devotees = success.data;
 					},
-					{
-						"name":"nirdosh"
-					},
-					{
-						"name":"parmarth"
-					}
-				];
-			});
-        }
+					function (error) {
+						console.log(error);
+					});
+		}
 	});
