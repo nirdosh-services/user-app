0a35e06ab2fb4dd12e483178278b19e19350c17a
Nirdosh <nirdosh@postcon.de>
2016-10-13 11:40:18 +0200

    updated

--- app/devotees/devotees.component.js
+++ app/devotees/devotees.component.js
@@ -2,10 +2,9 @@ angular.module('devotees')
 	.component('devotees',{
 		templateUrl: 'devotees/devotees.template.html',
         controller: function PhoneListController($scope, $http){
-			$http({
-					  method:'JSONP',
-					  url:"https://jsonplaceholder.typicode.com/posts"
-				  }
+			$http.get(
+					  "http://localhost:8080/devotee"
+
 
 			).then(
 				function (success) {
